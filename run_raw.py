89052b22c08b314cccd27094d3d4739aa2735940
Jackeline Brito <jackeline.brito@maistodos.com.br>
2024-02-09 20:24:15 -0300

    feat: Implementing App in Src

---
+++ run_raw.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify
+from barcode import Code128
+from barcode.writer import ImageWriter
+
+app = Flask(__name__) # criando um servidor em Flask
+
+#@app é um decorador
+@app.route('/create_tag', methods=["POST"])
+def create_tag():
+    body = request.json
+    product_code = body.get('product_code')
+
+    tag = Code128(product_code, writer=ImageWriter())
+    path_from_tag = f'{tag}'
+    # o caminho para a tag é a propria tag.
+    # Isso vai gerar uma especie de objeto com nome
+    tag.save(path_from_tag)
+
+    return jsonify({"tag_path": path_from_tag})
+
+if __name__ == '__main__': #para rodar servidor em flask
+    app.run(host='0.0.0.0', port=3000)
